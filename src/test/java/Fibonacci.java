387aac79f922c17e98049709389c7aeb61ea1b3f
William Walsh <william.stephen.walsh@gmail.com>
2024-06-29 22:10:44 +1000

    Add tests to fibonacci algo file.

--- src/test/java/Fibonacci.java
+++ src/test/java/Fibonacci.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Fibonacci {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+// Starting numbers -> 0, 1
+// 0+ 1 =1
+// 0, 1, 1, 2, 3, 5, 8, 13, 21
+public class Fibonacci {
+
+    private int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+
+
+    @Test
+    void fibonacciTest() {
+        assertEquals(0, fibonacci(0));
+        assertEquals(1, fibonacci(1));
+        assertEquals(1, fibonacci(2));
+        assertEquals(2, fibonacci(3));
+        assertEquals(3, fibonacci(4));
+        assertEquals(5, fibonacci(5));
+    }
 }
