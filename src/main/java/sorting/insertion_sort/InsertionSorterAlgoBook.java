c18eadf274b6154d5619cc04cde0e4c1ad6b929a
William Walsh <william.stephen.walsh@gmail.com>
2024-07-17 19:15:19 +1000

    Remove unnecessary loop iteration.

--- src/main/java/sorting/insertion_sort/InsertionSorterAlgoBook.java
+++ src/main/java/sorting/insertion_sort/InsertionSorterAlgoBook.java
@@ -6,7 +6,7 @@ public class InsertionSorterAlgoBook implements Sorter {
     @Override
     public int[] sort(int[] a) {
         int min, tmp;
-        for (int i = 0; i < a.length; i++) {
+        for (int i = 0; i < a.length-1; i++) {
             min = i;
             for (int j = i; j < a.length; j++) {
                 if (a[j] < a[min]) {
--- src/test/java/sorting/insertion_sort/InsertionSorterAlgoBookTest.java
+++ src/test/java/sorting/insertion_sort/InsertionSorterAlgoBookTest.java
@@ -9,9 +9,18 @@ class InsertionSorterAlgoBookTest {
     private static final String ASSERTION_ERROR_MESSAGE = "Arrays are not equal and therefore one is not sorted correctly.";
 
     @Test
-    void testInsertionSort() {
+    void insertionSortTest() {
         int[] arrToSort = {2, 1, 0, 3};
         int[] expectedArr = {0, 1, 2, 3};
+
+        new InsertionSorterAlgoBook().sort(arrToSort);
+        assertArrayEquals(expectedArr, arrToSort, ASSERTION_ERROR_MESSAGE);
+    }
+
+    @Test
+    void insertionSortLastElementTest() {
+        int[] arrToSort = {2, 3, 1};
+        int[] expectedArr = {1, 2, 3};
 
         new InsertionSorterAlgoBook().sort(arrToSort);
         assertArrayEquals(expectedArr, arrToSort, ASSERTION_ERROR_MESSAGE);
